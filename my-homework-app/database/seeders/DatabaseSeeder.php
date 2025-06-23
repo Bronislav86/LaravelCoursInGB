ae8a7336b963b1f84fa6c799877c74e53f2021f2
Bronislav <shevchenko-bg@yandex.ru>
2025-06-23 23:53:47 +0300

    was added ProductController

--- my-homework-app/database/seeders/DatabaseSeeder.php
+++ my-homework-app/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Product;
+use Database\Seeders\ProductsSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Product::factory(10)->create();
+        $this->call(ProductsSeeder::class);
     }
 }
