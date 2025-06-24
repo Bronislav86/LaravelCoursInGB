dd2da8a37eef92f19d8dcf32c44112dae3abdc16
Bronislav <shevchenko-bg@yandex.ru>
2025-06-25 01:04:46 +0300

    was added HW 14

--- voyager-project/config/voyager.php
+++ voyager-project/config/voyager.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Admin\Widgets\CategoriesWidget;
 use App\Admin\Widgets\NewsWidget;
+use App\Admin\Widgets\ProductsWidget;
 use TCG\Voyager\Widgets\UserDimmer;
 
 return [
@@ -147,8 +149,10 @@ return [
         ],
 
         'widgets' => [
-            UserDimmer::class,
+            //UserDimmer::class,
             NewsWidget::class,
+            ProductsWidget::class,
+            CategoriesWidget::class,
         ],
 
     ],
